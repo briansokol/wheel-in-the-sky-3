9e8b93c843c9026faab61c4f9fceaa5a945cdd12
Brian Sokol <bri@nsokol.net>
2025-04-18 00:16:00 -0400

    new winner modal

--- apps/web/src/pages/wheel-page.tsx
+++ apps/web/src/pages/wheel-page.tsx
@@ -1,10 +1,11 @@
-import { Button } from '@heroui/react';
+import { Button, Card, CardBody, CardFooter, CardHeader } from '@heroui/react';
 import { WheelManager } from '@repo/shared/classes/wheel-manager';
 import { DEFAULT_BACKGROUND_COLOR, DEFAULT_FOREGROUND_COLOR } from '@repo/shared/constants/colors';
 import { useDocumentTitle, useLockBodyScroll } from '@uidotdev/usehooks';
 import confetti from 'canvas-confetti';
 import { RefObject, useContext, useEffect, useRef, useState } from 'react';
 import { FaChevronDown } from 'react-icons/fa6';
+import { IoRemoveCircleOutline } from 'react-icons/io5';
 import { LuClipboardCopy } from 'react-icons/lu';
 import { useNavigate, useParams } from 'react-router';
 import { Banner } from '@/components/banner';
@@ -16,7 +17,7 @@ import { useDecodedConfig } from '@/hooks/config';
 import { useViewportWidth } from '@/hooks/viewport';
 import { calculateStartingRotation } from '@/utils/math';
 
-const WHEEL_PADDING = 20; // Padding of one side, applied to both sides
+const WHEEL_PADDING = 40; // Padding of one side, applied to both sides
 
 function getBanner(bannerRef: RefObject<HTMLCanvasElement | null>) {
     if (bannerRef.current === null) {
@@ -101,34 +102,58 @@ export default function WheelPage() {
         !isPending &&
         !isError &&
         segment && (
-            <main className="relative mt-16 flex items-center justify-center">
-                <div className="flex flex-col items-center text-center">
-                    {wheelManager?.config?.title && (
-                        <h1 className="text-base sm:text-xl">{wheelManager?.config?.title}</h1>
-                    )}
-                    <p
-                        className={`${hasWinner ? 'animate-float' : ''} flex items-center justify-center space-x-2 text-center text-xl font-bold sm:text-4xl`}
-                    >
-                        <span data-testid="winner" className="mx-4" aria-label="Winner">
-                            {segment?.name}
-                        </span>
-                    </p>
-                    <FaChevronDown className="mx-auto text-xl sm:text-4xl" />
+            <main className="min-h-screen">
+                <div className="absolute left-1/2 top-1/2 flex -translate-x-1/2 -translate-y-1/2 flex-col items-center text-center">
+                    <FaChevronDown className="relative top-[15px] z-10 mx-auto text-xl drop-shadow-lg sm:text-4xl" />
                     <Wheel
                         wheelManager={wheelManager}
                         radius={
-                            viewportWidth < 500 + WHEEL_PADDING * 2 ? `${viewportWidth - WHEEL_PADDING * 2}px` : '500px'
+                            viewportWidth > 500 + WHEEL_PADDING * 2 ? `${viewportWidth - WHEEL_PADDING * 2}px` : '500px'
                         }
                     />
                     {hasWinner && (
-                        <Button
-                            color="success"
-                            startContent={<LuClipboardCopy />}
-                            onPress={() => getBanner(bannerRef)}
-                            className="mt-4"
+                        <Card
+                            isBlurred
+                            className="absolute left-1/2 top-1/2 z-10 -translate-x-1/2 -translate-y-1/2 overflow-visible bg-background/50 dark:bg-default-100/50"
+                            shadow="lg"
                         >
-                            Copy Banner
-                        </Button>
+                            {wheelManager?.config?.title && (
+                                <CardHeader className="flex items-center justify-center">
+                                    <h1 className="text-base sm:text-xl">{wheelManager?.config?.title}</h1>
+                                </CardHeader>
+                            )}
+                            <CardBody className="overflow-visible">
+                                <p className="flex animate-float items-center justify-center space-x-2 overflow-visible text-center text-xl font-bold sm:text-4xl">
+                                    <span data-testid="winner" aria-label="Winner">
+                                        {segment?.name}
+                                    </span>
+                                </p>
+                            </CardBody>
+                            <CardFooter className="flex items-center justify-center">
+                                <Button
+                                    color="default"
+                                    radius="lg"
+                                    size="md"
+                                    variant="flat"
+                                    className="mx-1"
+                                    startContent={<LuClipboardCopy />}
+                                    onPress={() => getBanner(bannerRef)}
+                                >
+                                    Copy Banner
+                                </Button>
+                                <Button
+                                    color="default"
+                                    radius="lg"
+                                    size="md"
+                                    variant="flat"
+                                    className="mx-1"
+                                    startContent={<IoRemoveCircleOutline />}
+                                    onPress={() => null}
+                                >
+                                    Remove Winner
+                                </Button>
+                            </CardFooter>
+                        </Card>
                     )}
                 </div>
                 {hasWinner && (
